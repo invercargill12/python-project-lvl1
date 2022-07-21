c24e5d830f9b568f233b13fa7eeefd53f9c7cb88
Ivan Noev <ivnoyev@yandex.ru>
2022-07-22 02:06:46 +0300

    Installed flake8, setup.cfg, and fixed linter warnings

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,5 +4,3 @@ import prompt
 def welcome_user():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-
-
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -6,8 +6,6 @@ def main():
     print('Welcome to the Brain Games!')
     welcome_user()
 
+
 if __name__ == '__main__':
     main()
-
-
-
