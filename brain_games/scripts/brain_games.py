94b53f6abcac8ce2c25c5ac4b33c5629a0f24dc6
Ivan Noev <ivnoyev@yandex.ru>
2022-07-22 00:57:25 +0300

    Added welcome_user function via prompt library

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env/python
+from brain_games.cli import welcome_user
+
 
 def main():
     print('Welcome to the Brain Games!')
+    welcome_user()
 
 if __name__ == '__main__':
     main()
