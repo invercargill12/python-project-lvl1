ffb329ec09964b5719f781cf4ffcdf585cb6037a
Ivan Noev <ivnoyev@yandex.ru>
2022-09-07 01:07:59 +0300

    brain-gcd.py, gcd_game.py created

---
+++ brain_games/games/gcd_game.py
@@ -0,0 +1,16 @@
+import math
+from random import randint
+
+
+def common_divisor():
+    num_one = randint(2, 100)
+    num_two = randint(2, 100)
+    question = (f'''Find the greatest common divisor of given numbers.
+Question: {num_one} {num_two}''')
+    correct_answer = str(math.gcd(num_one, num_two))
+    return question, correct_answer
+
+
+
+
+
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/python
+
+from brain_games.games.gcd_game import common_divisor
+from brain_games.logic import logic
+
+
+def main():
+    logic(common_divisor)
+
+
+if __name__ == '__main__':
+    main()
+
