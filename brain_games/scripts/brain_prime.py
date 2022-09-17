ac9a6edc8cb20e2855f986741dc3bef2ba6ac98b
Ivan Noev <ivnoyev@yandex.ru>
2022-09-17 15:14:17 +0300

    added player in README.md and pushed technical changes

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env/python
+
+from brain_games.games.prime_game import prime_number_check
+from brain_games.logic import logic
+
+
+def main():
+    logic(prime_number_check)
+
+
+if __name__ == '__main__':
+    main()
