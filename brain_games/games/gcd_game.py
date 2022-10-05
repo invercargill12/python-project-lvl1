b1f0ea8b92e4c17395c1007cdfaa308c3339a563
Ivan Noev <ivnoyev@yandex.ru>
2022-10-06 00:54:13 +0500

    post-review fix of gcd_game.py

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -2,10 +2,14 @@ import math
 from random import randint
 
 
-def common_divisor():
-    num_one = randint(2, 100)
-    num_two = randint(2, 100)
-    question = (f'''Find the greatest common divisor of given numbers.
-Question: {num_one} {num_two}''')
+LOWER_LIMIT = 2
+UPPER_LIMIT = 50
+DESCRIPTION = 'Find the greatest common divisor of given numbers.'
+
+
+def get_round_data():
+    num_one = randint(LOWER_LIMIT, UPPER_LIMIT)
+    num_two = randint(LOWER_LIMIT, UPPER_LIMIT)
+    question = f'{num_one} {num_two}'
     correct_answer = str(math.gcd(num_one, num_two))
     return question, correct_answer
