2e3119801d247ae1bda58b2d019a2a5bdd005f0f
Ivan Noev <ivnoyev@yandex.ru>
2022-10-06 00:53:42 +0500

    post-review fix of even_game.py

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -1,12 +1,20 @@
 from random import randint
 
 
-def is_even():
-    random_number = randint(1, 200)
-    if random_number % 2 == 0:
+LOWER_LIMIT = 1
+UPPER_LIMIT = 200
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def get_round_data():
+    random_number = randint(LOWER_LIMIT, UPPER_LIMIT)
+    if is_even(random_number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
-    question = (f'''Answer "yes" if the number is even, otherwise answer "no".
-Question: {random_number}''')
+    question = random_number
     return question, correct_answer
+
+
+def is_even(random_number):
+    return random_number % 2 == 0
