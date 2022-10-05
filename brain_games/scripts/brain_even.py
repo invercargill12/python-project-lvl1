2daae53273121693cbc2e0a62936b132ece5ad93
Ivan Noev <ivnoyev@yandex.ru>
2022-10-06 00:55:44 +0500

    post-review fix of even game script

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env/python
 
-from brain_games.games.even_game import is_even
+from brain_games.games import even_game
 from brain_games.logic import logic
 
 
 def main():
-    logic(is_even)
+    logic(even_game)
 
 
 if __name__ == '__main__':
