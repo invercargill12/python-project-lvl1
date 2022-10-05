182e08ee239ee929979872574a7ccb712d370da5
Ivan Noev <ivnoyev@yandex.ru>
2022-10-06 00:06:22 +0500

    refactoring calc_game.py: constants, magic numbers

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -1,12 +1,15 @@
 from random import randint
 from random import choice
 
+LOWER_LIMIT = 1
+UPPER_LIMIT = 30
+DESCRIPTION = 'What is the result of the expression?'
 
-def calc():
+
+def get_round_data():
     operator = choice(['*', '+', '-'])
-    num_one = randint(1, 30)
-    num_two = randint(1, 10)
-    question = (f'''What is the result of the expression?
-Question: {num_one} {operator} {num_two}''')
+    num_one = randint(LOWER_LIMIT, UPPER_LIMIT)
+    num_two = randint(LOWER_LIMIT, UPPER_LIMIT)
+    question = f'{num_one} {operator} {num_two}'
     correct_answer = str(eval(f'{num_one}{operator}{num_two}'))
     return question, correct_answer
