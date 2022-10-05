dde23d3ecf7173bbbb70139956e420b015575250
Ivan Noev <ivnoyev@yandex.ru>
2022-10-06 00:55:57 +0500

    post-review fix of gcd game script

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env/python
 
-from brain_games.games.gcd_game import common_divisor
+from brain_games.games import gcd_game
 from brain_games.logic import logic
 
 
 def main():
-    logic(common_divisor)
+    logic(gcd_game)
 
 
 if __name__ == '__main__':
