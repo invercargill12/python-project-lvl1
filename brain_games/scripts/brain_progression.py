d707cf435f619a5403851f28533f5f4c249cc101
Ivan Noev <ivnoyev@yandex.ru>
2022-10-06 00:56:33 +0500

    post-review fix of progression game script

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env/python
 
-from brain_games.games.progression_game import progression
+from brain_games.games import progression_game
 from brain_games.logic import logic
 
 
 def main():
-    logic(progression)
+    logic(progression_game)
 
 
 if __name__ == '__main__':
