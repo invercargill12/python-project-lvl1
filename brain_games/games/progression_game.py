3b9e2d538afad79f2ed714b35635b63eb104438a
Ivan Noev <ivnoyev@yandex.ru>
2022-10-06 00:55:07 +0500

    post-review fix of progression_game.py

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -1,20 +1,26 @@
 from random import randint
 
 
-def progression():
+LOWER_LIMIT = 1
+START_UPPER_LIMIT = 20
+STEP_UPPER_LIMIT = 10
+LENGTH = 10
+DESCRIPTION = 'What number is missing in the progression?'
+
+
+def get_round_data():
     list = []
-    start = randint(1, 20)  # выбираем начальное число прогрессии
-    step = randint(1, 10)  # выбираем рандомный шаг прогрессии
-    end = start + (10 * step)  # выбираем конец прогрессии
+    start = randint(LOWER_LIMIT, START_UPPER_LIMIT)
+    step = randint(LOWER_LIMIT, STEP_UPPER_LIMIT)
+    end = start + (LENGTH * step)
 
     for index in range(start, end, step):
         list.append(str(index))
 
     limit = len(list) - 1
 
-    correct_answer_index = randint(1, limit)
+    correct_answer_index = randint(LOWER_LIMIT, limit)
     correct_answer = list[correct_answer_index]
     list[correct_answer_index] = ".."
-    question = (f'''What number is missing in the progression?
-Question: {' '.join(list)}''')
+    question = ' '.join(list)
     return question, correct_answer
