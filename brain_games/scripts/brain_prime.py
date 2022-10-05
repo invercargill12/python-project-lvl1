4325f092fcc4f7dffd7ddc50a74a1147b3014f0e
Ivan Noev <ivnoyev@yandex.ru>
2022-10-06 00:56:16 +0500

    post-review fix of prime game script

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env/python
 
-from brain_games.games.prime_game import prime_number_check
+from brain_games.games import prime_game
 from brain_games.logic import logic
 
 
 def main():
-    logic(prime_number_check)
+    logic(prime_game)
 
 
 if __name__ == '__main__':
