1ee345a657e5ea7e613e35b02be18dee24e1ff37
Ivan Noev <ivnoyev@yandex.ru>
2022-10-16 21:17:27 +0500

    progression_game.py 2nd review fix

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -4,25 +4,26 @@ from random import randint
 LOWER_LIMIT = 1
 START_UPPER_LIMIT = 20
 STEP_UPPER_LIMIT = 10
+MIN_LENGTH = 5
+MAX_LENGTH = 12
 DESCRIPTION = 'What number is missing in the progression?'
 
 
-def get_round_data():
+def get_progression():
     start = randint(LOWER_LIMIT, START_UPPER_LIMIT)
     step = randint(LOWER_LIMIT, STEP_UPPER_LIMIT)
-    length = 10
-    progression = get_progression(start, step, length)
-    numbers_list = []
-
-    for index in progression:
-        numbers_list.append(str(index))
-    correct_answer_index = randint(LOWER_LIMIT, len(numbers_list) - 1)
-    correct_answer = numbers_list[correct_answer_index]
-    numbers_list[correct_answer_index] = ".."
-    question = ' '.join(numbers_list)
-    return question, correct_answer
-
-
-def get_progression(start, step, length):
+    length = randint(MIN_LENGTH, MAX_LENGTH)
     end = start + (length * step)
+    numbers_list = []
+    for index in range(start, end, step):
+        numbers_list.append(index)
     return range(start, end, step)
+
+
+def get_round_data():
+    progression = list(get_progression())
+    correct_answer_index = randint(LOWER_LIMIT, len(progression) - 1)
+    correct_answer = str(progression[correct_answer_index])
+    progression[correct_answer_index] = ".."
+    question = ' '.join(map(str, progression))
+    return question, correct_answer
