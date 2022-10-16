926afe55ef2c6a747ec62322128cc79b8d0b3ba1
Ivan Noev <ivnoyev@yandex.ru>
2022-10-16 21:22:37 +0500

    brain_gcd.py 2nd review fix

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env/python
 
 from brain_games.games import gcd_game
-from brain_games.logic import logic
+from brain_games.logic import launch
 
 
 def main():
-    logic(gcd_game)
+    launch(gcd_game)
 
 
 if __name__ == '__main__':
