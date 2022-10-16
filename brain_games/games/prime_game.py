b5f0110d01cf715114d8b6f36594eedf914c7a47
Ivan Noev <ivnoyev@yandex.ru>
2022-10-16 21:17:09 +0500

    prime_game.py 2nd review fix

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -9,16 +9,20 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(random_number):
+
     if random_number < 2:
         return False
+
     for i in range(RANGE_START, random_number, RANGE_STEP):
         if random_number % i == 0:
             return False
+
     return True
 
 
 def get_round_data():
     random_number = randint(LOWER_LIMIT, UPPER_LIMIT)
+
     if is_prime(random_number):
         correct_answer = 'yes'
     else:
