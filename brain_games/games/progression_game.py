aa91374f2689a25a316de74fe2e27737ea8cff7a
Ivan Noev <ivnoyev@yandex.ru>
2022-10-17 16:26:31 +0500

    progression_game.py last fix

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -9,19 +9,19 @@ MAX_LENGTH = 12
 DESCRIPTION = 'What number is missing in the progression?'
 
 
-def get_progression():
-    start = randint(LOWER_LIMIT, START_UPPER_LIMIT)
-    step = randint(LOWER_LIMIT, STEP_UPPER_LIMIT)
-    length = randint(MIN_LENGTH, MAX_LENGTH)
+def get_progression(start, step, length):
     end = start + (length * step)
     numbers_list = []
     for index in range(start, end, step):
         numbers_list.append(index)
-    return range(start, end, step)
+    return numbers_list
 
 
 def get_round_data():
-    progression = list(get_progression())
+    start = randint(LOWER_LIMIT, START_UPPER_LIMIT)
+    step = randint(LOWER_LIMIT, STEP_UPPER_LIMIT)
+    length = randint(MIN_LENGTH, MAX_LENGTH)
+    progression = get_progression(start, step, length)
     correct_answer_index = randint(LOWER_LIMIT, len(progression) - 1)
     correct_answer = str(progression[correct_answer_index])
     progression[correct_answer_index] = ".."
